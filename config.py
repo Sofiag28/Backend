7708234fa60baa40396651e6bf403b37eb2cfdd9
Sofiag28 <laurasofiag1012@gmail.com>
2025-10-15 18:08:51 -0500

    pymysql

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
 def get_db_connection():
-    return pymysql.connector.connect(
+    return pymysql.connect(
         host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
