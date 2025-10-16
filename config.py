7e3e9eecfee32f8cac55d2eeb27cefe744370346
Sofiag28 <laurasofiag1012@gmail.com>
2025-10-15 23:22:26 -0500

    certificado

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import pymysql
 from dotenv import load_dotenv
 import os
 
-# Cargar variables de entorno desde el archivo .env
+# Cargar variables de entorno desde .env
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
@@ -10,6 +10,7 @@ DB_PORT = int(os.getenv("DB_PORT"))
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
+SSL_CA = "ca-certificate.crt"  # Ruta al certificado CA
 
 def get_db_connection():
     return pymysql.connect(
@@ -17,5 +18,6 @@ def get_db_connection():
         user=DB_USER,
         password=DB_PASSWORD,
         database=DB_NAME,
-        port=DB_PORT
+        port=DB_PORT,
+        ssl={'ca': SSL_CA}  # Aquí indicamos el certificado SSL
     )
